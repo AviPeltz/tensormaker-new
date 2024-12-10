8b9421a259fd50028cc98e1a6d9c34bd548bec77
AviPeltz <aj.peltz@gmail.com>
2024-12-10 10:41:05 -0800

    projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,9 @@ import { Layout } from "@/components/layout"
 export default function ProjectsPage(){
     return(
         <Layout>
-            <div>Projects</div>
+            <section className="mb-12">
+                <h1 className="text-3xl font-semibold mb-4">Projects</h1>
+            </section>
         </Layout>
     );
 }
